2bf22e243ab3e39ec2832aef1addfbf8b2edae9a
Kjasn <3384148601@qq.com>
2023-10-09 17:49:33 +0800

    用户注册api

---
+++ init.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+var mysqlLogger logger.Interface
+
+func init() {
+	// 连接数据库
+	//drivername := "mysql"
+	username := "root"
+	password := "8520"
+	host := "localhost"
+	port := "3306"
+	database := "gin_essential"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		username, password, host, port, database)
+
+	// dsn := "root:8520@tcp(127.0.0.1:3306)/ginEssential?charset=utf8mb4&parseTime=True&loc=L=True&loc=Local"
+
+	mysqlLogger = logger.Default.LogMode(logger.Info)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: mysqlLogger,
+	})
+
+	DB = db
+	//db, err := gorm.Open(drivername, dsn)
+
+	if err != nil {
+		panic("数据库连接失败: " + err.Error())
+	}
+
+	fmt.Println("数据库连接成功")
+	db.AutoMigrate(&User{})
+
+}
--- main.go
+++ main.go
@@ -3,26 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
-func getRandomString(sz int) string {
-	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*")
-	var ret = make([]byte, sz)
-	rand.Seed(time.Now().Unix())
-	for i := range ret {
-		ret[i] = letters[rand.Intn(len(letters))]
-	}
-
-	return string(ret)
+type User struct {
+	gorm.Model
+	Name      string `json:"name" gorm:"type:varchar(20);not null"`
+	Telephone string `json:"telephone" gorm:"type:varchar(11);not null;unique"`
+	Pwd       string `json:"pwd" gorm:"type:varchar(20);not null"`
 }
 
 func main() {
+
 	r := gin.Default()
 
-	r.GET("/api/auth/register", func(ctx *gin.Context) {
+	r.POST("/api/auth/register", func(ctx *gin.Context) {
 		// 获取数据
 		// 要求  昵称 手机号 密码
 		name := ctx.PostForm("name")
@@ -46,6 +44,19 @@ func main() {
 			return // 终止
 		}
 
+		// 检验 手机号是否以及存在
+		if existTelephone(telephone) {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"msg": "手机号已经存在"})
+			return // 终止
+		}
+
+		// 通过校验后 创建用户
+		DB.Debug().Create(&User{
+			Name:      name,
+			Telephone: telephone,
+			Pwd:       pwd,
+		})
+
 		// 返回结果
 		ctx.JSON(http.StatusOK, gin.H{
 			"msg": "注册成功",
@@ -56,4 +67,24 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func getRandomString(sz int) string {
+	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*")
+	var ret = make([]byte, sz)
+	rand.Seed(time.Now().Unix())
+	for i := range ret {
+		ret[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(ret)
+}
+
+func existTelephone(telephone string) bool {
+	var user User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
 }
