561b10f56c679baa7e9bd09d27382c4d871aa667
Kjasn <3384148601@qq.com>
2023-10-10 18:05:40 +0800

    2023/10/10 增加login api

--- controller/UserController.go
+++ controller/UserController.go
@@ -6,6 +6,7 @@ import (
 	"Kjasn/ginEssential/util"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
@@ -19,7 +20,6 @@ func existTelephone(telephone string) bool {
 }
 
 func Register(ctx *gin.Context) {
-	//ctx.HTML(http.StatusOK, "register.html", nil)  // why???
 
 	// 获取数据
 	// 要求  昵称 手机号 密码
@@ -30,17 +30,19 @@ func Register(ctx *gin.Context) {
 	// 数据验证
 	if len(name) == 0 { // 为获取到昵称
 		name = util.GetRandomString(10) // randString
-		fmt.Println(name)
 	}
 
 	if len(telephone) != 11 {
 		ctx.JSON(http.StatusUnprocessableEntity,
-			gin.H{"msg": "手机号必须为11位"})
+			gin.H{"code": 422, "msg": "手机号必须为11位"})
 		return // 终止
 	}
 
 	if len(pwd) < 6 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"msg": "密码至少为6位"})
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码至少为6位",
+		})
 		return // 终止
 	}
 
@@ -50,15 +52,61 @@ func Register(ctx *gin.Context) {
 		return // 终止
 	}
 
+	fmt.Printf(name, telephone, pwd)
+
 	// 通过校验后 创建用户
+
+	// 加密密码
+	hashPwd, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密失败"})
+		return // 终止
+	}
+
 	common.DB.Create(&model.User{
 		Name:      name,
 		Telephone: telephone,
-		Pwd:       pwd,
+		Pwd:       string(hashPwd), // 保存加密后的密码
 	})
+
+	if common.DB.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "注册失败"})
+		fmt.Println(common.DB.Error)
+		return // 终止
+	}
 
 	// 返回结果
 	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "注册成功",
+		"code": 200,
+		"msg":  "注册成功",
+	})
+}
+
+func Login(ctx *gin.Context) {
+	// 通过 手机号 + 密码 登录
+	telephone := ctx.PostForm("telephone")
+	pwd := ctx.PostForm("pwd")
+
+	// 数据验证
+	var user model.User
+	common.DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "该手机号码未注册"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Pwd), []byte(pwd)); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码错误"})
+		return
+	}
+
+	// 生成token
+	token := "111"
+
+	// 返回结果
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
 	})
 }
