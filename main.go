907ba64aabb4b309091704d87c6fd01eff0bdbce
Kjasn <3384148601@qq.com>
2023-10-23 11:01:34 +0800

    2023/10/23 连接数据库时文件中读取配置

--- main.go
+++ main.go
@@ -4,18 +4,32 @@ import (
 	"Kjasn/ginEssential/common"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
-	common.InitDB()
+	InitConfig()    // 读取配置文件
+	common.InitDB() // 初始化数据库
 
 	r := gin.Default()
 
 	r = CollectRouter(r)
 
-	err := r.Run(":80")
+	port := viper.GetString("server.port")
+	err := r.Run(":%s", port)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
 
+func InitConfig() {
+	workDir, err := os.Getwd() // 获取当前工作目录
+	if err != nil {
+		panic(err)
+	}
+
+	viper.SetConfigName("application")       // 配置文件名称
+	viper.SetConfigType("yml")               // 配置文件类型
+	viper.AddConfigPath(workDir + "/config") // 配置文件路径
 }
