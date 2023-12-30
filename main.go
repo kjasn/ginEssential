6970111ee098eb7acfcf0ab3a45ce91aad20d461
Kjasn <3384148601@qq.com>
2023-12-31 02:27:33 +0800

    2023/12/31 修复连接数据库的bug

--- main.go
+++ main.go
@@ -3,21 +3,25 @@ package main
 import (
 	"Kjasn/ginEssential/common"
 	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
-	"os"
 )
 
 func main() {
-	InitConfig()    // 读取配置文件
+	InitConfig() // 读取配置文件
+	fmt.Println("reading config file successfully")
 	common.InitDB() // 初始化数据库
+	fmt.Println("init db successfully")
 
 	r := gin.Default()
 
 	r = CollectRouter(r)
 
 	port := viper.GetString("server.port")
-	err := r.Run(":%s", port)
+	fmt.Println(port)
+	err := r.Run(":" + port)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -30,6 +34,9 @@ func InitConfig() {
 	}
 
 	viper.SetConfigName("application")       // 配置文件名称
-	viper.SetConfigType("yml")               // 配置文件类型
+	viper.SetConfigType("yaml")              // 配置文件类型
 	viper.AddConfigPath(workDir + "/config") // 配置文件路径
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
 }
